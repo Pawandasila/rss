8a26e33eca04f92f84f9f268102e08e8801e2938
Pawandasila <pawandasila06@gmail.com>
2025-12-12 09:19:27 +0530

    fixed pages

--- src/app/(main)/become-member/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const BecomeMemberLayout = ({
-  children,
-}: Readonly<{ children: React.ReactNode }>) => {
-  return <div className="p-24">{children}</div>;
-};
-
-export default BecomeMemberLayout;
